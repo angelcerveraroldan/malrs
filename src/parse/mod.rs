e9d71305cd470c440e5843e34e2779c94c5c3b06
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2024-08-20 12:31:46 +0100

    Mini-parser combinator

--- src/parse/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod parse;
-
-#[derive(Debug, PartialEq)]
-pub enum ParsingError {
-    PatternNotFound(String),
-    CannotParseAnEmptyString,
-}
-
-pub type ParserRes<A, E = ParsingError> = std::result::Result<(A, String), E>;
